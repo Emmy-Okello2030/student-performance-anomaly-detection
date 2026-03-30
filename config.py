33728e548006150b4c2e570b95834f8954ff9b87
Emmy-Okello2030 <emyokellow@gmail.com>
2026-03-30 08:27:37 +0300

    Fix: Add DATABASE_PATH to config

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ DATA_DIR = BASE_DIR / 'data'
 RAW_DATA_DIR = DATA_DIR / 'raw'
 PROCESSED_DATA_DIR = DATA_DIR / 'processed'
 MODEL_DIR = BASE_DIR / 'models'
+DATABASE_PATH = PROCESSED_DATA_DIR / 'anomaly_detection.db'
+
 
 # Model parameters
 RF_PARAMS = {
